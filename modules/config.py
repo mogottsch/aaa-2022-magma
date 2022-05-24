3801dcca46ab3172ff42c7b17aae8381e1f4ce1a
Moritz <mgottsc5@smail.uni-koeln.de>
2022-05-24 12:41:30 +0200

    Calculate trips & availability for all resolutions and time intervals

--- modules/config.py
+++ modules/config.py
@@ -11,31 +11,30 @@ UNPACKED_TRIPS_DIR_PATH = os.path.join(DATA_DIR_PATH, "leipzig")
 
 PROCESSED_DATA_DIR_PATH = os.path.join(DATA_DIR_PATH, "processed")
 ORIGINAL_DATA_MERGED_PATH = os.path.join(
-    PROCESSED_DATA_DIR_PATH, "original_data_merged.pkl"
+    PROCESSED_DATA_DIR_PATH, "original_data_merged.parquet"
 )
-TRIPS_PATH = os.path.join(PROCESSED_DATA_DIR_PATH, "trips.pkl")
-RELOCATIONS_PATH = os.path.join(PROCESSED_DATA_DIR_PATH, "relocations.pkl")
-MOVEMENTS_PATH = os.path.join(PROCESSED_DATA_DIR_PATH, "movements.pkl")
+TRIPS_PATH = os.path.join(PROCESSED_DATA_DIR_PATH, "trips.parquet")
+RELOCATIONS_PATH = os.path.join(PROCESSED_DATA_DIR_PATH, "relocations.parquet")
+MOVEMENTS_PATH = os.path.join(PROCESSED_DATA_DIR_PATH, "movements.parquet")
 
-TRIPS_GROUPED_SPATIAL_PATH = os.path.join(
-    PROCESSED_DATA_DIR_PATH, "trips_grouped_spatial.pkl"
-)
 TRIPS_GROUPED_SPATIO_TEMPORAL_PATH = os.path.join(
-    PROCESSED_DATA_DIR_PATH, "trips_grouped_spatio_temporal.pkl"
+    PROCESSED_DATA_DIR_PATH, "trips_grouped_spatio_temporal.parquet"
 )
 RELOCATIONS_GROUPED_SPATIO_TEMPORAL_PATH = os.path.join(
-    PROCESSED_DATA_DIR_PATH, "relocations_grouped_spatio_temporal.pkl"
+    PROCESSED_DATA_DIR_PATH, "relocations_grouped_spatio_temporal.parquet"
 )
 MOVEMENTS_GROUPED_SPATIO_TEMPORAL_PATH = os.path.join(
-    PROCESSED_DATA_DIR_PATH, "movements_grouped_spatio_temporal.pkl"
+    PROCESSED_DATA_DIR_PATH, "movements_grouped_spatio_temporal.parquet"
 )
 
-AVAILABILITY_PATH = os.path.join(PROCESSED_DATA_DIR_PATH, "availability.pkl")
+AVAILABILITY_PATH = os.path.join(PROCESSED_DATA_DIR_PATH, "availability.parquet")
 
-POIS_PATH = os.path.join(PROCESSED_DATA_DIR_PATH, "pois.pkl")
-HEXAGON_WITH_POIS_PATH = os.path.join(PROCESSED_DATA_DIR_PATH, "hexagons_with_pois.pkl")
+POIS_PATH = os.path.join(PROCESSED_DATA_DIR_PATH, "pois.parquet")
+HEXAGON_WITH_POIS_PATH = os.path.join(
+    PROCESSED_DATA_DIR_PATH, "hexagons_with_pois.parquet"
+)
 
-MODEL_DATA_PATH = os.path.join(PROCESSED_DATA_DIR_PATH, "model_data.pkl")
+MODEL_DATA_PATH = os.path.join(PROCESSED_DATA_DIR_PATH, "model_data.parquet")
 
 REPO_DATA_DIR_PATH = os.path.join(DATA_DIR_PATH, "repo_data")
 FLEXZONE_GEOJSON_PATH = os.path.join(REPO_DATA_DIR_PATH, "leipzig.geojson")
@@ -43,6 +42,8 @@ FLEXZONE_GEOJSON_PATH = os.path.join(REPO_DATA_DIR_PATH, "leipzig.geojson")
 
 ### Configurations ###
 H3_RESOLUTION = 8
+CALC_H3_RESOLUTIONS = [8, 9]
 
 N_TIME_INTERVALS = 4
 TIME_INTERVAL_LENGTH = 24 / N_TIME_INTERVALS
+CALC_TIME_INTERVAL_LENGTHS = [1, 2, 6, 24]
