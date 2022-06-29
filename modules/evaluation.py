855eb42ede1223139bb4396ac1ea5c0f444ed950
Moritz <mgottsc5@smail.uni-koeln.de>
2022-06-29 15:48:55 +0200

    Replace mape as evalutation criteria

--- modules/evaluation.py
+++ modules/evaluation.py
@@ -1,9 +1,11 @@
 from sklearn.metrics import (
     mean_squared_error,
     mean_absolute_error,
+    accuracy_score,
 )
 
 # ---------------------------------------------------------------------------------------------------
+
 
 def mean_average_percentage_error(y_true, y_pred):
     return mean_absolute_error(y_true, y_pred) / y_true.mean()
@@ -14,9 +16,19 @@ def root_mean_squared_error(y_true, y_pred):
 
 
 def get_evaluation_metrics(y_true, y_pred, prefix):
+    y_true_non_zero = y_true[y_true != 0]
+    y_true_is_zero = y_true == 0
+
+    y_pred_non_zero = y_pred[y_true != 0]
+    y_pred_is_zero = y_pred == 0
+
     return {
-        prefix+'_mse': mean_squared_error(y_true, y_pred),
-        prefix+'_mae': mean_absolute_error(y_true, y_pred),
-        prefix+'_mape': mean_average_percentage_error(y_true, y_pred),
-        prefix+'_rmse': root_mean_squared_error(y_true, y_pred),
+        prefix + "_mse": mean_squared_error(y_true, y_pred),
+        prefix + "_rmse": root_mean_squared_error(y_true, y_pred),
+        prefix + "_mae": mean_absolute_error(y_true, y_pred),
+        prefix
+        + "_non_zero_mape": mean_average_percentage_error(
+            y_true_non_zero, y_pred_non_zero
+        ),
+        prefix + "_zero_accuracy": accuracy_score(y_true_is_zero, y_pred_is_zero),
     }
